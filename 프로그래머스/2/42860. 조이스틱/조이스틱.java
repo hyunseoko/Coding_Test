d6f831c5dde12b3e7c18c378114fd8640156a488
고현서 <128693161+hyunseoko@users.noreply.github.com>
2025-09-10 16:35:28 +0900

    [level 2] Title: 조이스틱, Time: 0.06 ms, Memory: 87.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(String name) {
+        int answer = 0;
+        int len = name.length();
+        int move = len-1;
+        
+        for (char c : name.toCharArray()) {
+            answer += Math.min(c-'A', 'Z'-c+1);
+        }
+        
+        for (int i=0; i<len; i++) {
+            int next = i+1;
+            while (next<len && name.charAt(next)=='A') next++;
+            move = Math.min(move, i*2+(len-next));
+            move = Math.min(move, (len-next)*2+i);
+        }
+        
+        answer += move;
+        return answer;
+    }
+}
