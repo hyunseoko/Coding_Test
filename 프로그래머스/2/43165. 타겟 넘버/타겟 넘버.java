bda63fad55715475cd877e673785510e49550879
고현서 <128693161+hyunseoko@users.noreply.github.com>
2025-09-30 17:02:34 +0900

    [level 2] Title: 타겟 넘버, Time: 6.73 ms, Memory: 96.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,24 +1,14 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[] numbers, int target) {
-        int n = numbers.length;
-        int[] remain = new int[n + 1];
-        
-        for (int i = n - 1; i >= 0; i--) {
-            remain[i] = remain[i + 1] + Math.abs(numbers[i]);
-        }
-        
-        return dfs(0, 0, numbers, target, remain);
+        int answer = 0;
+        answer = dfs(numbers, 0, 0, target);
+        return answer;
     }
-    private int dfs(int i, int sum, int[] numbers, int target, int[] remain) {
-        if (i == numbers.length) return sum == target ? 1 : 0;
-        
-        int need = target - sum;
-        if (Math.abs(need) > remain[i]) return 0;
-        
-        int a = dfs(i + 1, sum+numbers[i], numbers, target, remain);
-        int b = dfs(i + 1, sum-numbers[i], numbers, target, remain);
-        return a + b;
+    int dfs(int[] numbers, int n, int sum, int target) {
+        if (n == numbers.length) {
+            if (sum == target) return 1;
+            return 0;
+        }
+        return dfs(numbers, n + 1, sum + numbers[n], target) + dfs(numbers, n + 1, sum - numbers[n], target);
     }
 }
