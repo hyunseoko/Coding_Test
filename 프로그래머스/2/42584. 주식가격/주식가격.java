9798d2371d783ab7f562563ed44f7c0f1977755d
고현서 <128693161+hyunseoko@users.noreply.github.com>
2025-10-03 17:10:10 +0900

    [level 2] Title: 주식가격, Time: 47.01 ms, Memory: 70.5 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,19 +1,24 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] prices) {
         int n = prices.length;
         int[] answer = new int[n];
-        
+        Deque<Integer> st = new ArrayDeque<>(); 
+
         for (int i = 0; i < n; i++) {
-            int count = 0;
-            int j = i + 1;
-            while (j < n) {
-                count++;
-                if (prices[j] < prices[i]) break;
-                j++;
+            while (!st.isEmpty() && prices[i] < prices[st.peek()]) {
+                int idx = st.pop();
+                answer[idx] = i - idx;
             }
-            answer[i] = count;
+            st.push(i);
         }
-        
+
+        while (!st.isEmpty()) {
+            int idx = st.pop();
+            answer[idx] = (n - 1) - idx;
+        }
+
         return answer;
     }
 }
