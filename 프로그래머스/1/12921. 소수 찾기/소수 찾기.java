d9071c58a3730eb6765624838364b912305270a8
고현서 <128693161+hyunseoko@users.noreply.github.com>
2025-10-09 15:16:46 +0900

    [level 1] Title: 소수 찾기, Time: 20.43 ms, Memory: 54.5 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -1,18 +1,23 @@
+import java.util.*;
+
 class Solution {
     public int solution(int n) {
-        if (n < 2) return 0;
-        boolean[] composite = new boolean[n + 1];
+        int answer = 0;
+        boolean[] prime = new boolean[n + 1];
+        prime[0] = true; prime[1] = true;
         
-        for (long i = 2; i * i <= n; i++) {
-            if (!composite[(int)i]) {
-                for (long j = i * i; j <= n; j += i) {
-                    composite[(int)j] = true;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (!prime[i]) {
+                for (int j = i * i; j <= n; j += i) {
+                    prime[j] = true;
                 }
             }
         }
         
-        int cnt = 0;
-        for (int i = 2; i <= n; i++) if (!composite[i]) cnt++;
-        return cnt;
+        for (int i = 1; i <= n; i++) {
+            if (!prime[i]) answer++;
+        }
+        
+        return answer;
     }
 }
