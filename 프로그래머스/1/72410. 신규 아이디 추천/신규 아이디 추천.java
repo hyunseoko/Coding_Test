ee0256045ed977c5026b2f0f0fb6f61f10cc0977
고현서 <128693161+hyunseoko@users.noreply.github.com>
2026-06-13 17:52:10 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.48 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,37 +1,6 @@
 class Solution {
     public String solution(String new_id) {
-        StringBuilder sb = new StringBuilder();
-
-        new_id = new_id.toLowerCase();
-
-        for (char c : new_id.toCharArray()) {
-            if (Character.isLowerCase(c) || Character.isDigit(c)
-                    || c == '-' || c == '_' || c == '.') {
-
-                if (c == '.') {
-                    if (sb.length() == 0) continue;
-                    if (sb.charAt(sb.length() - 1) == '.') continue;
-                }
-
-                sb.append(c);
-            }
-        }
-
-        if (sb.length() > 0 && sb.charAt(sb.length() - 1) == '.') sb.deleteCharAt(sb.length() - 1);
-
-        if (sb.length() == 0) sb.append("a");
-
-        if (sb.length() > 15) {
-            sb.setLength(15);
-
-            if (sb.charAt(sb.length() - 1) == '.') {
-                sb.deleteCharAt(sb.length() - 1);
-            }
-        }
-
-        char last = sb.charAt(sb.length() - 1);
-        while (sb.length() < 3) sb.append(last);
-
-        return sb.toString();
+        String answer = "";
+        return answer;
     }
 }
