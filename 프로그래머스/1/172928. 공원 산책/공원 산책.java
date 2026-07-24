d7a2e604f7b6238b8f854d5ca58870a75d058363
고현서 <128693161+hyunseoko@users.noreply.github.com>
2026-07-24 17:14:49 +0900

    [level 1] Title: 공원 산책, Time: 3.48 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.java
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.java
@@ -2,63 +2,71 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] park, String[] routes) {
-        int h = park.length;
-        int w = park[0].length();
-        int[] answer = new int[2];
+        int h = park.length; //높이
+        int w = park[0].length(); //너비
+        int x = 0; int y = 0;
         boolean[][] arr = new boolean[h][w];
+        
+        // 위아래좌우
+        int[] dy = {-1, 1, 0, 0};
+        int[] dx = {0, 0, -1, 1};
         
         for (int i = 0; i < h; i++) {
             String[] p = park[i].split("");
             for (int j = 0; j < w; j++) {
                 if (p[j].equals("S")) {
-                    answer[0] = i;
-                    answer[1] = j;
+                    y = i;
+                    x = j;
                 }
                 else if (p[j].equals("X")) arr[i][j] = true;
             }
         }
         
-        for (String route : routes) {
-            String[] s = route.split(" ");
-            int count = Integer.parseInt(s[1]);
-            boolean x = true;
+        for (String route: routes) {
+            String[] r = route.split(" ");
+            int count = Integer.parseInt(r[1]);
             
-            switch(s[0]){
+            switch(r[0]) {
                 case "E":
-                    if (answer[1] + count < w) {
-                        for (int i = answer[1]; i <= answer[1] + count; i++) {
-                            if (arr[answer[0]][i]) x = false;
-                        }
-                        if (x) answer[1] += count;
+                    if (canMove(arr, count, dx[3], dy[3], x, y, h, w)) {
+                        x += count;
                     }
                     break;
                 case "W":
-                    if (answer[1] - count >= 0) {
-                        for (int i = answer[1]; i >= answer[1] - count; i--) {
-                            if (arr[answer[0]][i]) x = false;
-                        }
-                        if (x) answer[1] -= count;
+                    if (canMove(arr, count, dx[2], dy[2], x, y, h, w)) {
+                        x -= count;
                     }
                     break;
                 case "S":
-                    if (answer[0] + count < h) {
-                        for (int i = answer[0]; i <= answer[0] + count; i++) {
-                            if (arr[i][answer[1]]) x = false;
-                        }
-                        if (x) answer[0] += count;
+                    if (canMove(arr, count, dx[1], dy[1], x, y, h, w)) {
+                        y += count;
                     }
                     break;
                 case "N":
-                    if (answer[0] - count >= 0) {
-                        for (int i = answer[0]; i >= answer[0] - count; i--) {
-                            if (arr[i][answer[1]]) x = false;
-                        }
-                        if (x) answer[0] -= count;
+                    if (canMove(arr, count, dx[0], dy[0], x, y, h, w)) {
+                        y -= count;
                     }
                     break;
             }
         }
         
-        return answer;
+        return new int[] {y, x};
+    }
+    
+    private boolean canMove(boolean[][] arr, int count, int dx, int dy, int x, int y, int h, int w) {
+        boolean b = true;
+        
+        for (int i = 0; i < count; i++) {
+            if (x + dx < w && x + dx >= 0 && y + dy < h && y + dy >= 0) {
+                x += dx;
+                y += dy;
+                if (arr[y][x]) {
+                    b = false;
+                    break;
+                }
+            } else b = false;
+        }
+        
+        return b;
     }
 }
