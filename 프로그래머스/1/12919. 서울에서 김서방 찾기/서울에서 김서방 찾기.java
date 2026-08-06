8d13710cc220d7828395dec1701b43644b891bec
고현서 <128693161+hyunseoko@users.noreply.github.com>
2026-08-06 13:22:40 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 2.75 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -3,9 +3,12 @@ class Solution {
         int idx = 0;
         
         for (int i = 0; i < seoul.length; i++) {
-            if (seoul[i].equals("Kim")) idx = i;
+            if (seoul[i].equals("Kim")) {
+                idx = i;
+                break;
+            }
         }
         
-        return "김서방은 " + Integer.toString(idx) + "에 있다";
+        return "김서방은 " + idx + "에 있다";
     }
 }
