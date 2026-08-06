e98161239406436a76394fdbd870a34ba957ef47
고현서 <128693161+hyunseoko@users.noreply.github.com>
2026-08-06 11:01:17 +0900

    [level 2] Title: 숫자의 표현, Time: 0.86 ms, Memory: 56.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 1;
+        int sum = 0;
+        
+        for (int i = 1; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                sum += j;
+                if (sum >= n) {
+                    if (sum == n) answer++;
+                    sum = 0;
+                    break;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
