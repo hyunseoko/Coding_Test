79cd27906c4361b4141135bdcf90537b48fb1554
고현서 <128693161+hyunseoko@users.noreply.github.com>
2026-08-07 15:41:02 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.03 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -2,17 +2,11 @@ import java.util.*;
 
 class Solution {
     public int solution(int n) {
-        int answer = 0;
         int count = Integer.bitCount(n);
         
-        while (true) {
-            int count2 = Integer.bitCount(++n);
-            if (count == count2) {
-                answer = n;
-                break;
-            }
+        while (Integer.bitCount(++n) != count) {
         }
         
-        return answer;
+        return n; 
     }
 }
