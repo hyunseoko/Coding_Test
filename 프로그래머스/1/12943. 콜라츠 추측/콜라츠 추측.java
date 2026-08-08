1b23dc85d5419e838b24ab1704b152a19f62724b
고현서 <128693161+hyunseoko@users.noreply.github.com>
2026-08-08 13:57:11 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,15 +1,15 @@
 class Solution {
-    public int solution(long num) {
+    public int solution(int num) {
+        long n = (long) num;
         int answer = -1;
         
-        for (long i = 0; i < 500; i++) {
-            if (num == 1) {
-                answer = (int)i;
+        for (int i = 0; i < 500; i++) {
+            if (n == 1) {
+                answer = i;
                 break;
             }
             
-            else if (num % 2 == 0) num /= 2;
-            else if (num % 2 != 0) num = (num * 3) + 1;
+            n = (n % 2 == 0) ? n / 2 : n * 3 + 1;
         }
         
         return answer;
