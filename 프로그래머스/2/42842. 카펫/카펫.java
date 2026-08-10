4eff5a3d706a744faec610ca73a107eddcbd3a0e
고현서 <128693161+hyunseoko@users.noreply.github.com>
2026-08-11 01:09:08 +0900

    [level 2] Title: 카펫, Time: 3.32 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,22 +1,15 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int x = 0;
-        int y = 0;
         long sum = brown + yellow;
-        
-        // brown = 2(x + y - 2)
-        
+                
         for (int i = 1; i < brown / 2; i++) {
             for (int j = i; j < brown / 2; j++) {
-                if (i + j - 2 == brown / 2) {
-                    if (i * j == sum) {
-                        x = j;
-                        y = i;
-                    }
+                if (i + j - 2 == brown / 2 && i * j == sum) {
+                    return new int[] {j, i};
                 }
             }
         }
         
-        return new int[] {x, y};
+        return null;
     }
 }
